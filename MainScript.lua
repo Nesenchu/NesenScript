b64f72a6b84c372fe804e056773159a782e2ecf1
Nesenchu <nesenchuk53@gmail.com>
2025-05-18 18:04:01 +0200

    initial upload 2

--- MainScript.lua
+++ MainScript.lua
@@ -1,34 +1,34 @@
--- MainScript.lua
+-- Загружаем модули с GitHub
+local repo = "https://raw.githubusercontent.com/Nesenchu/NesenScript/master/Modules/"
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local BindManager     = loadstring(game:HttpGet(repo .. "BindManager.lua"))()
+local GUIManager      = loadstring(game:HttpGet(repo .. "GUIManager.lua"))()
+local PlayerManager   = loadstring(game:HttpGet(repo .. "PlayerManager.lua"))()
+local TeleportManager = loadstring(game:HttpGet(repo .. "TeleportManager.lua"))()
+local NoclipManager   = loadstring(game:HttpGet(repo .. "NoclipManager.lua"))()
+
+-- Службы
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 
--- Загружаем модули
-local BindManager = require(script.Modules.BindManager)
-local GUIManager = require(script.Modules.GUIManager)
-local PlayerManager = require(script.Modules.PlayerManager)
-local TeleportManager = require(script.Modules.TeleportManager)
-local NoclipManager = require(script.Modules.NoclipManager)
-
--- Запуск GUI
+-- Инициализация GUI
 local screenGui = GUIManager:CreateGUI()
 
--- Запуск разделов GUI
+-- Инициализация разделов
 PlayerManager:Init(screenGui)
 TeleportManager:Init(screenGui)
 BindManager:Init(screenGui)
 NoclipManager:Init(screenGui)
 
--- Управление переключением интерфейса
+-- Переключение интерфейса на L
 UserInputService.InputBegan:Connect(function(input, gpe)
 	if not gpe and input.KeyCode == Enum.KeyCode.L then
 		screenGui.Enabled = not screenGui.Enabled
 	end
 end)
 
--- Обновление Noclip движения
+-- Обновление noclip в каждом кадре
 RunService.RenderStepped:Connect(function(dt)
 	NoclipManager:Update(dt)
 end)
